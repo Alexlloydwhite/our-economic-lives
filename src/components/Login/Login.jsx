d7dfd17f3656a7e92e040ad7a9d1496ed98bd75b
Alex White <alexlloydwhite@gmail.com>
2021-06-21 10:24:39 -0500

    adding basic login form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,3 +1,123 @@
+// MUI
+import {
+    Typography,
+    Paper,
+    makeStyles,
+    Avatar,
+    TextField,
+    Button,
+    Link,
+    Grid
+} from '@material-ui/core/';
+// React
+import { useDispatch, useSelector } from 'react-redux';
+import { useState } from 'react';
+import { useHistory } from 'react-router';
+//styles
+// Styles
+const useStyles = makeStyles((theme) => ({
+    layout: {
+        width: 'auto',
+        marginLeft: theme.spacing(2),
+        marginRight: theme.spacing(2),
+        [theme.breakpoints.up(600 + theme.spacing(2) * 2)]: {
+            width: 600,
+            marginLeft: 'auto',
+            marginRight: 'auto',
+        },
+    },
+    paper: {
+        marginTop: theme.spacing(3),
+        marginBottom: theme.spacing(3),
+        padding: theme.spacing(2),
+        [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
+            marginTop: theme.spacing(6),
+            marginBottom: theme.spacing(6),
+            padding: theme.spacing(3),
+        },
+    },
+}));
+
 export default function Login() {
-    return <h2>yo</h2>
+    const classes = useStyles();
+    const history = useHistory();
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const errors = useSelector(store => store.errors)
+    const dispatch = useDispatch();
+
+    const login = (event) => {
+        event.preventDefault();
+
+        if (username && password) {
+            dispatch({
+                type: 'LOGIN',
+                payload: {
+                    username: username,
+                    password: password,
+                },
+            });
+        } else {
+            dispatch({ type: 'LOGIN_INPUT_ERROR' });
+        }
+    }; // end login
+    return (
+            <main className={classes.layout}>
+                <Paper className={classes.paper}>
+                    <Typography
+                        component="h3"
+                        variant="h3"
+                        align="center"
+                        gutterBottom
+                        id="header"
+                    >
+                        Our Economic Lives
+                    </Typography>
+                    <form className={classes.form} onSubmit={login} noValidate>
+                        {errors.loginMessage && (
+                            <h3 className="alert" role="alert">
+                                {errors.loginMessage}
+                            </h3>
+                        )}
+                        {/* Input for Username */}
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="Username"
+                            label="Username"
+                            name="Username"
+                            autoComplete="email"
+                            value={username}
+                            onChange={(event) => setUsername(event.target.value)}
+                        />
+                        {/* input for password */}
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            type="password"
+                            name="password"
+                            label="Password"
+                            required
+                            autoComplete="current-password"
+                            value={password}
+                            onChange={(event) => setPassword(event.target.value)}
+                            style={{ marginBottom: 20 }}
+                        />
+                        {/* submit button */}
+                        <Button
+                            type="submit"
+                            variant="contained"
+                            color="primary"
+                            fullWidth
+                        >
+                            Log In
+                        </Button>
+                    </form>
+                </Paper>
+            </main>
+    )
 }
