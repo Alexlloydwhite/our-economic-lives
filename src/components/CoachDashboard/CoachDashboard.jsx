a7bc98aacd015d8996e056dcbff725df61d63ec0
Alex White <alexlloydwhite@gmail.com>
2021-06-23 15:46:49 -0500

    button to open dialog

--- src/components/CoachDashboard/CoachDashboard.jsx
+++ src/components/CoachDashboard/CoachDashboard.jsx
@@ -1,21 +1,30 @@
+// MUI imports
 import { 
     Typography,
     Button,
-    Table,
-    TableBody,
-    TableCell,
-    TableContainer,
-    TableHead,
-    TableRow
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle
  } from '@material-ui/core';
+// React Imports
+import { useState } from 'react';
 
 export default function CoachDashboard() {
+    const [open, setOpen] = useState(false);
+
     return (
         <div style={{ textAlign: 'center' }}>
             <Typography>
                 You may have a maximum of 8 clients on your team
             </Typography>
-            <Button>Invite a new client to your team</Button>
+            <Button onClick={() => setOpen(true)}>Invite a new client to your team</Button>
+            <Dialog open={open} onClose={() => setOpen(false)}>
+                <DialogTitle>
+                    Add a new client to your team
+                </DialogTitle>
+            </Dialog>
         </div>
     );
 };
