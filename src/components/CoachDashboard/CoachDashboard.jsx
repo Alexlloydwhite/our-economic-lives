bfa17f91d2a519687f22b1c7bab689e9c58acb57
Alex White <alexlloydwhite@gmail.com>
2021-06-23 10:30:42 -0500

    coach dashboard is only visable to coaches

--- src/components/CoachDashboard/CoachDashboard.jsx
+++ src/components/CoachDashboard/CoachDashboard.jsx
@@ -1,3 +1,14 @@
+import { useSelector } from 'react-redux';
+
 export default function CoachDashboard() {
-    return <h1 style={{textAlign: 'center'}}>This is the coach's dashboard</h1>
-}
+    const user = useSelector(store => store.user);
+    if (user.authorization === 2) {
+        return (
+            <h1 style={{ textAlign: 'center' }}>This is the coach's dashboard</h1>
+        );
+    } else {    
+        return (
+            <h1 style={{ textAlign: 'center' }}>This is a restricted area for coach's only</h1>
+        );
+    };
+};
