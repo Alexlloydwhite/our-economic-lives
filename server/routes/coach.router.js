e7627e8478e3136978f471a20b1aad073b1cb65b
Alex White <alexlloydwhite@gmail.com>
2021-06-24 09:41:10 -0500

    adding get router to grab list of coach's clients

--- server/routes/coach.router.js
+++ server/routes/coach.router.js
@@ -2,8 +2,24 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-app.get('/', (req,res) => {
-    res.send('yo');
+// Handles GET request for users that are
+// associated with a particular coach
+router.get('/', (req, res) => {
+    // Get coach id from req.user
+    const coachId = req.user.id;
+    const queryText = `
+    SELECT * 
+    FROM "user" u
+    WHERE u.coach_id=$1;`;
+    pool
+        .query(queryText, [coachId])
+        .then(() => {
+            res.send(result.rows);
+        })
+        .catch(err => {
+            res.sendStatus(500);
+            console.log(`IN /api/coach GET router. ${err}`);
+        });
 });
 
 module.exports = router;
