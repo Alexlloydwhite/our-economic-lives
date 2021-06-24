599c45572325d580c3e1bd70fbd9f3f23457011a
Alex White <alexlloydwhite@gmail.com>
2021-06-24 09:47:14 -0500

    adding useeffect hook and saga to grab client data

--- src/components/CoachDashboard/ClientTable.jsx
+++ src/components/CoachDashboard/ClientTable.jsx
@@ -1,4 +1,15 @@
+import { useEffect } from "react"
+import { useDispatch } from "react-redux"
+
 export default function ClientTable() {
+    const dispatch = useDispatch();
+    // On page load, grab the client
+    // data associated with coach
+    useEffect(() => {
+        dispatch({
+            type: 'FETCH_CLIENTS'
+        })
+    }, [])
     return (
         <h5>Client Table Here!</h5>
     )
