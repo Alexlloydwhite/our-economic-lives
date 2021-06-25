7d5e341ad454a292e87331b10c33b22742e3e201
Alex White <alexlloydwhite@gmail.com>
2021-06-25 14:22:18 -0500

    updating create clietn saga to reflect new end point

--- src/redux/sagas/CreateClient.saga.js
+++ src/redux/sagas/CreateClient.saga.js
@@ -6,7 +6,7 @@ export default function* createClient(action) {
   try {
     console.log(`IN createClient Saga`);
     // passes the username and password from the payload to the server
-    yield axios.post('/api/user/create', action.payload);
+    yield axios.post('/api/coach/create-client', action.payload);
     yield put({ type: 'FETCH_CLIENTS' });
   } catch (error) {
     yield put({ type: 'REGISTRATION_FAILED' });
