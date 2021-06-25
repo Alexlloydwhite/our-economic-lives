3b4234bc81fe462589b52efffcd5a5c9f5924d1b
johnturner4004 <johnturner4004@gmail.com>
2021-06-25 16:17:33 -0400

    make footer sticky

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,21 @@
+import { makeStyles } from '@material-ui/core';
 import React from 'react';
+import OELtitle from '../../resources/images/OELtitle.png'
 
+const useStyles = makeStyles({
+  footer: {
+    textAlign: 'center',
+    position: 'sticky',
+    bottom: 10,
+  },
+})
 
 export default function Footer() {
-  return <footer>&copy; Our Economic Lives</footer>
+  const classes = useStyles();
+
+  return (
+  <footer className={classes.footer}>&copy; 2021 <img src={OELtitle} height="16pt" /></footer>
+  )
 }
 
 
