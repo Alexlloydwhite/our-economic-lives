d0c908a5ffc976890c9860d8ca728b63145b2154
SamMoudry <sammoudry@gmail.com>
2021-06-25 11:20:37 -0500

    finally the upload router is working, switched if statement to a switch statement

--- src/components/AdminDashboard/AdminDashboard.jsx
+++ src/components/AdminDashboard/AdminDashboard.jsx
@@ -5,20 +5,20 @@ import {useSelector, useDispatch} from 'react-redux';
 
 export default function AdminDashboard() {
     const dispatch = useDispatch();
-    const [careerPath, setCareerPath] = useState();
+    const [careerPath, setCareerPath] = useState(0);
+    let routerPath = '/api/upload/' + careerPath;
     const setCareerPaths = useSelector(store => store.career_path)
     useEffect(() => {
         dispatch({type: 'FETCH_CAREER_PATH'})
     }, [dispatch])
 
 
-
-
     return <>
             <h3>I'm an admin hehehehehehehehehe</h3>
-            <form>
+            <form action = {routerPath} method="POST" enctype="multipart/form-data">
                 <input type="file" name="file" accept=".csv" />
-                <select name="Career Path" onChange={(event) => setCareerPath(event.target.id)}>
+                <select name="Career Path" onChange={(event) => setCareerPath(event.target.value)}>
+                    <option>Choose a Career Path</option>
                     {setCareerPaths ? setCareerPaths.map((path, i) => {
                         return (<option key={i} value={path.id}>{path.name}</option>)
                     }): <option>No Career Paths</option>}
