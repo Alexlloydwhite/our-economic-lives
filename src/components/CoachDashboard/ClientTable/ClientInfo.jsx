5d8470fe91671ded3eff08f73e86d0b35a2d737e
Alex White <alexlloydwhite@gmail.com>
2021-06-25 10:06:55 -0500

    added dialog to display client info

--- src/components/CoachDashboard/ClientTable/ClientInfo.jsx
+++ src/components/CoachDashboard/ClientTable/ClientInfo.jsx
@@ -6,24 +6,35 @@ import {
     DialogContent,
     DialogContentText,
     DialogTitle,
+    List,
+    ListItem,
+    ListItemText,
+    ListItemAvatar,
+    IconButton
 } from '@material-ui/core'
+import PersonIcon from '@material-ui/icons/Person';
+import PhoneIcon from '@material-ui/icons/Phone';
+import HomeIcon from '@material-ui/icons/Home';
+import WorkIcon from '@material-ui/icons/Work';
+import ChangeHistoryIcon from '@material-ui/icons/ChangeHistory';
+import CloseIcon from '@material-ui/icons/Close';
 // React
 import { useState } from 'react';
-import { useDispatch } from 'react-redux';
 
 export default function ClientInfo({ clientList, client }) {
-    const dispatch = useDispatch();
     // State of client detail dialog
-    const [clientDetailsClicked, setClientDetailsClicked] = useState({});
+    const [clientDetailsClicked, setClientDetailsClicked] = useState(null);
     const [openDialog, setOpenDialog] = useState(false);
+
     const handleOpenClientInfo = (id) => {
         // Open the dialog
         setOpenDialog(true);
-        dispatch({
-            type: 'FETCH_CLIENT_BY_ID',
-            payload: id
-        });
+        // filter client list to grab client with matching ID
+        const clientClicked = (clientList.filter((client) => client.id === id));
+        setClientDetailsClicked(clientClicked);
     }
+
+
     return (
         <>
             <MenuItem
@@ -33,13 +44,64 @@ export default function ClientInfo({ clientList, client }) {
             </MenuItem>
             <Dialog
                 open={openDialog}
-                onClose={() => setOpenDialog(false)}
             >
                 <DialogTitle>
+                    <IconButton onClick={() => setOpenDialog(false)}>
+                        <CloseIcon />
+                    </IconButton>
                     {"Client Info"}
                 </DialogTitle>
                 <DialogContent>
-
+                    <List>
+                        {clientDetailsClicked ?
+                            clientDetailsClicked.map((idx) => (
+                                <div key={idx.id}>
+                                    <ListItem>
+                                        <ListItemAvatar>
+                                            <PersonIcon />
+                                        </ListItemAvatar>
+                                        <ListItemText>
+                                            {idx.first_name}{' '}{idx.last_name}
+                                        </ListItemText>
+                                    </ListItem>
+                                    <ListItem>
+                                        <ListItemAvatar>
+                                            <PhoneIcon />
+                                        </ListItemAvatar>
+                                        <ListItemText>
+                                            {idx.phone_number}
+                                        </ListItemText>
+                                    </ListItem>
+                                    <ListItem>
+                                        <ListItemAvatar>
+                                            <HomeIcon />
+                                        </ListItemAvatar>
+                                        <ListItemText>
+                                            {idx.city}
+                                        </ListItemText>
+                                    </ListItem>
+                                    <ListItem>
+                                        <ListItemAvatar>
+                                            <WorkIcon />
+                                        </ListItemAvatar>
+                                        <ListItemText>
+                                            {idx.current_profession}
+                                        </ListItemText>
+                                    </ListItem>
+                                    <ListItem>
+                                        <ListItemAvatar>
+                                            <ChangeHistoryIcon />
+                                        </ListItemAvatar>
+                                        <ListItemText>
+                                            {idx.desired_career}
+                                        </ListItemText>
+                                    </ListItem>
+                                </div>
+                        ))
+                        :
+                        null
+                        }
+                    </List>
                 </DialogContent>
             </Dialog>
         </>
