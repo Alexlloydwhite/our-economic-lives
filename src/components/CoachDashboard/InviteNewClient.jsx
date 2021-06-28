527aada79ca6e8b84869f7a5bd53e31a94c4216b
Alex White <alexlloydwhite@gmail.com>
2021-06-28 12:58:38 -0500

    adding paper texture to coach dashboard

--- src/components/CoachDashboard/InviteNewClient.jsx
+++ src/components/CoachDashboard/InviteNewClient.jsx
@@ -8,12 +8,37 @@ import {
     DialogContent,
     DialogContentText,
     DialogTitle,
+    Paper,
+    Grid
 } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 // React Imports
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+// Styles
+const useStyles = makeStyles((theme) => ({
+    container: {
+        width: 'auto',
+        marginTop: theme.spacing(3),
+        marginBottom: theme.spacing(3),
+        marginLeft: theme.spacing(1),
+        marginRight: theme.spacing(1),
+        [theme.breakpoints.up(1000 + theme.spacing(2) * 2)]: {
+            width: 1000,
+            marginLeft: 'auto',
+            marginRight: 'auto',
+        },
+    },
+    table: {
+        marginRight: theme.spacing(2),
+        marginLeft: theme.spacing(2),
+        maxWidth: 600,
+        padding: 15
+    },
+}));
 
 export default function InviteNewClient({ activeClientList }) {
+    const classes = useStyles();
     const dispatch = useDispatch();
     const errors = useSelector(store => store.errors);
     // State for dialog. Is it open or closed?
@@ -59,22 +84,45 @@ export default function InviteNewClient({ activeClientList }) {
     return (
         <div style={{ textAlign: 'center', marginTop: 10 }}>
             {/* Page Title */}
-            <Typography variant="h5">
-                You have {activeClientList.length} / 8 clients on your team
+            <Typography
+                variant="h3"
+                gutterBottom
+            >
+                Coach Dashboard
             </Typography>
-            {/* Btn to invite new client */}
-            {activeClientList.length < 8 ?
-                <Button
-                    onClick={() => setOpen(true)}
-                    variant="outlined"
+            <Grid
+                container
+                className={classes.container}
+                direction="row"
+                justify="center"
+                alignItems="center"
+            >
+                <Grid
+                    item
+                    component={Paper}
+                    className={classes.table}
                 >
-                    Invite a new client to your team
-                </Button>
-                :
-                <Typography variant="h6">
-                    Your client list is full
-                </Typography>
-            }
+                    <Typography
+                        variant="h6"
+                        gutterBottom
+                    >
+                        You have {activeClientList.length} / 8 clients on your team
+                    </Typography>
+                    {/* Btn to invite new client */}
+                    {activeClientList.length < 8 ?
+                        <Button
+                            onClick={() => setOpen(true)}
+                            variant="outlined"
+                        >
+                            Invite a new client to your team
+                        </Button>
+                        :
+                        <Typography variant="h6">
+                            Your client list is full
+                        </Typography>
+                    }
+                </Grid>
+            </Grid>
             {/* Dialog */}
             <Dialog open={open} onClose={handleClose}>
                 {/* Title */}
