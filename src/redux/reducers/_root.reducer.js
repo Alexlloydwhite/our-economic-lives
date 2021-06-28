c304daef23f759fafb85c37b7dff46337b8b52ee
Alex White <alexlloydwhite@gmail.com>
2021-06-28 10:51:13 -0500

    connecting coach reducer to root reducer

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -4,7 +4,7 @@ import user from './user.reducer';
 import clients from './clients.reducer';
 import career_path from './careerPath.reducer.js';
 import detail from './details.reducer';
-
+import coaches from './coaches.reducer';
 
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
@@ -16,6 +16,7 @@ const rootReducer = combineReducers({
   errors, // contains registrationMessage and loginMessage
   user, // will have an id and username if someone is logged in
   clients, // holds array of client data for each coach
+  coaches,
   career_path, //holds an array of all the career path data
   detail,
 });
