63559559e638dbcde05bd29460d4e091ce45af9c
Karsten Jepsen <kjepsen86@gmail.com>
2021-06-28 14:28:42 -0500

    Recommended blocks displaying from DB

--- src/components/Home/RecommendedBlocks.jsx
+++ src/components/Home/RecommendedBlocks.jsx
@@ -1,11 +1,14 @@
+// React
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+// M-UI
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import React from 'react';
-import { useDispatch } from 'react-redux';
-import { useHistory } from 'react-router-dom';
+
 
 const useStyles = makeStyles((theme) => ({
     content: { 
@@ -14,12 +17,12 @@ const useStyles = makeStyles((theme) => ({
     },
   }));
 
-export default function PyramidTier() {
+export default function RecommendedBlocks() {
 
     const classes = useStyles();
     const history = useHistory();
     const dispatch = useDispatch();
-    // const recommendations = useSelector((store) => store);
+    const blocks = useSelector((store) => store.clientBlocks);
 
     // send user to recommended block detail using history, params
     const handleClick = (id) => {
@@ -28,35 +31,29 @@ export default function PyramidTier() {
         history.push(`/blockDetail/${id}`);  
     }
 
-    const sample = [
-        {
-            block: 'Demonstrating Sensitivity/Empathy',
-          },
-          {
-            block: 'Demonstrating insight into behavior',
-          },
-          {
-            block: 'Maintaining open relationships',
-          },
-    ];
-
     return (
         <Card >
             <CardContent>
+                {/* Helper Text */}
                 <Typography variant="h5" >
-                    Your Recommended Building Blocks
+                    Recommended Building Blocks:
                 </Typography>
-                {sample.map((recommended, i) => (
-                    <Button 
-                        key={i}
-                        variant="contained" 
-                        color="primary" 
-                        className={classes.content}
-                        onClick={() => handleClick(i)}
-                        >
-                        {recommended.block}
-                    </Button>
-                ))}
+                {/* Iterate through client blocks to check if recommended, and render if true */}
+                {blocks.map((block, i) => {
+                    if( block.is_recommended === true ) {
+                        return (
+                            <Button 
+                            key={i}
+                            variant="contained" 
+                            color="primary" 
+                            className={classes.content}
+                            onClick={() => handleClick(i)}
+                            >
+                            {block.name}
+                        </Button>
+                        )
+                    }
+                })}
             </CardContent>
         </Card>
     )
