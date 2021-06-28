87fc8267ca348e90671baa89cd2a203d562a2a5f
Alex White <alexlloydwhite@gmail.com>
2021-06-28 15:05:16 -0500

    adding MUI to building blocks form

--- src/components/ManagePyramid/AddBlocks.jsx
+++ src/components/ManagePyramid/AddBlocks.jsx
@@ -31,54 +31,54 @@ export default function AddBlocks() {
             alignItems="center"
         >
             <Grid item>
+                <Typography>
+                    Add New Building Block
+                </Typography>
                 <form action={routerPath} method="POST" encType="multipart/form-data">
-                    <Button
-                        variant="contained"
-                        component="label"
-                        color="primary"
-                        startIcon={<CloudUploadIcon />}
-                    >
-                        Upload CSV
-                        <input
-                            type="file"
-                            name="file"
-                            accept=".csv"
-                            hidden
-                        />
-                    </Button>
-                    <FormControl
-                        variant="outlined"
-                        required
-                    >
-                        <InputLabel>Career Path</InputLabel>
-                        <Select
-                            name="Career Path"
-                            value={careerPath || ''}
-                            onChange={(e) => setCareerPath(e.target.value)}
+                    <Grid item>
+                        <FormControl
+                            variant="outlined"
+                            required
+                            fullWidth
                         >
-                            {setCareerPaths ? setCareerPaths.map((path) => {
-                                return (<MenuItem key={path.id} value={path.id}>{path.name}</MenuItem>)
-                            }) : <MenuItem>No Career Paths</MenuItem>}
-                        </Select>
-                    </FormControl>
-                    <Button type="submit">
-                        Add Blocks
-                    </Button>
+                            <InputLabel>Career Path</InputLabel>
+                            <Select
+                                name="Career Path"
+                                value={careerPath || ''}
+                                onChange={(e) => setCareerPath(e.target.value)}
+                            >
+                                {setCareerPaths ? setCareerPaths.map((path) => {
+                                    return (<MenuItem key={path.id} value={path.id}>{path.name}</MenuItem>)
+                                }) : <MenuItem>No Career Paths</MenuItem>}
+                            </Select>
+                        </FormControl>
+                    </Grid>
+                    <Grid item>
+                        <Button
+                            variant="contained"
+                            component="label"
+                            color="primary"
+                            startIcon={<CloudUploadIcon />}
+                        >
+                            Upload CSV
+                            <input
+                                type="file"
+                                name="file"
+                                accept=".csv"
+                                hidden
+                            />
+                        </Button>
+                        <Button
+                            type="submit"
+                            color="primary"
+                            variant="contained"
+                            fullWidth
+                        >
+                            Add Blocks
+                        </Button>
+                    </Grid>
                 </form>
             </Grid>
         </Grid>
     )
 }
-
-{/* <div>
-<form action={routerPath} method="POST" encType="multipart/form-data">
-    <input type="file" name="file" accept=".csv" />
-    <select name="Career Path" onChange={(event) => setCareerPath(event.target.value)}>
-        <option>Choose a Career Path</option>
-        {setCareerPaths ? setCareerPaths.map((path, i) => {
-            return (<option key={i} value={path.id}>{path.name}</option>)
-        }) : <option>No Career Paths</option>}
-    </select>
-    <button type="submit">Add Blocks</button>
-</form>
-</div> */}
