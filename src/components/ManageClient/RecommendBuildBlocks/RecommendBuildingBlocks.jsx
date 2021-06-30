403e486e2d8f2c52c19e4583cbe00632f58cfb21
Alex White <alexlloydwhite@gmail.com>
2021-06-30 18:25:56 -0500

    refactored if statement to be a ternary

--- src/components/ManageClient/RecommendBuildBlocks/RecommendBuildingBlocks.jsx
+++ src/components/ManageClient/RecommendBuildBlocks/RecommendBuildingBlocks.jsx
@@ -5,12 +5,7 @@ export default function RecommendBuildingBlocks({ block }) {
     const [color, setColor] = useState('default');
 
     const handleBlockChipClick = (block) => {
-        console.log('clicked!', block.id);
-        if (color === 'default') {
-            setColor('primary');
-        } else {
-            setColor('default');
-        }
+        color === 'default' ? setColor('primary') : setColor('default');
     }
 
     return (
