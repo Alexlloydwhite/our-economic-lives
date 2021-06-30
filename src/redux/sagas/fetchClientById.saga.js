8ee8781fa0fdd64da5d3db136909b0979187e873
Alex White <alexlloydwhite@gmail.com>
2021-06-30 16:05:28 -0500

    resolving spelling error

--- src/redux/sagas/fetchClientById.saga.js
+++ src/redux/sagas/fetchClientById.saga.js
@@ -1,12 +1,12 @@
 import { put } from 'redux-saga/effects';
 import axios from 'axios';
 
-export default function* fetchClientId(action) {
+export default function* fetchClientById(action) {
     try {
         // Get data from end point
         const clients = yield axios.get(`/api/coach/`);
-        console.log(`IN fetchClientId saga. Response from GET request ${clients.data}`);
+        console.log(`IN fetchClientById saga. Response from GET request ${clients.data}`);
     } catch (err) {
-        console.log(`IN fetchClientId saga. Error on GET request ${err}`);
+        console.log(`IN fetchClientById saga. Error on GET request ${err}`);
     }
 }
