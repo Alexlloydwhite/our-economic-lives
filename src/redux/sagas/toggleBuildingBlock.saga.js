b2268725d8b68e62de42275ac6d9366a15689a78
Alex White <alexlloydwhite@gmail.com>
2021-06-30 18:52:57 -0500

    removing bad comment

--- src/redux/sagas/toggleBuildingBlock.saga.js
+++ src/redux/sagas/toggleBuildingBlock.saga.js
@@ -1,7 +1,6 @@
 import { put } from 'redux-saga/effects';
 import axios from 'axios';
 
-// worker Saga: will be fired on "REGISTER" actions
 export default function* toggleBuildingBlock(action) {
     try {
         console.log(`IN toggleBuildingBlock saga`);
