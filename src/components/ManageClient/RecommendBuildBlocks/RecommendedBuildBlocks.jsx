420c577a2b27ad5e65ca088a8002737c0704bd25
Alex White <alexlloydwhite@gmail.com>
2021-07-01 13:32:03 -0500

    adding usestyles hook

--- src/components/ManageClient/RecommendBuildBlocks/RecommendedBuildBlocks.jsx
+++ src/components/ManageClient/RecommendBuildBlocks/RecommendedBuildBlocks.jsx
@@ -1,11 +1,29 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from "react-router";
-import { Typography } from '@material-ui/core';
 
 import BuildingBlockChip from './BuildingBlockChip';
 
+import { Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+// Styles
+const useStyles = makeStyles((theme) => ({
+    container: {
+        width: 'auto',
+        marginTop: theme.spacing(3),
+        marginBottom: theme.spacing(3),
+        marginLeft: theme.spacing(1),
+        marginRight: theme.spacing(1),
+        [theme.breakpoints.up(1000 + theme.spacing(2) * 2)]: {
+            width: 1000,
+            marginLeft: 'auto',
+            marginRight: 'auto',
+        },
+    }
+}));
+
 export default function RecommendedBuildBlocks() {
+    const classes = useStyles();
     const dispatch = useDispatch();
     const params = useParams();
     const pyramidData = useSelector(store => store.clientPyramid);
@@ -18,7 +36,7 @@ export default function RecommendedBuildBlocks() {
     }, [])
 
     return (
-        <div>
+        <div className={classes.container}>
             <div style={{ textAlign: 'center' }}>
                 <Typography>
                     Recommend Building Blocks
