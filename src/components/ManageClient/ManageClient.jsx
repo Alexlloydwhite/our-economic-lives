3dedb6b53287c078925e2df3793e21de0eddc0b9
Alex White <alexlloydwhite@gmail.com>
2021-07-01 17:12:47 -0500

    adding crit review and rec build blocks to same level

--- src/components/ManageClient/ManageClient.jsx
+++ src/components/ManageClient/ManageClient.jsx
@@ -1,10 +1,12 @@
 import RecommendedBuildBlocks from "./RecommendBuildBlocks/RecommendedBuildBlocks";
+import CoachCritExpReview from './CoachCritExpReview';
 
 export default function ManageClient() {
  
     return (
         <div>
             <RecommendedBuildBlocks />
+            <CoachCritExpReview />
         </div>
     );
 }
