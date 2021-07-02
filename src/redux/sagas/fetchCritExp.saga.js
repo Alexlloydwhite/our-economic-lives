08e902044648c85d0a3dd050a9b4741a175f3e6b
Alex White <alexlloydwhite@gmail.com>
2021-07-01 21:12:16 -0500

    adding reducer to hold crit exp data

--- src/redux/sagas/fetchCritExp.saga.js
+++ src/redux/sagas/fetchCritExp.saga.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 export default function* fetchCritExp(action) {
     try {
         const allCritExp = yield axios.get(`/api/coach/critical-experience/${action.id}`);
-        
+        yield put({ type: 'SET_CRIT_EXP', payload: allCritExp.data });
     } catch (err) {
         console.log(`In fetchCritExp: ${err}`);
     }
