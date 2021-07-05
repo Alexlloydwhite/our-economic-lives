97fe0798f473d51cae9942850accc5d63f4f1b9c
Alex White <alexlloydwhite@gmail.com>
2021-07-05 11:20:41 -0500

    adding textfield and button to send message

--- src/components/Messaging/SendMessageForm.jsx
+++ src/components/Messaging/SendMessageForm.jsx
@@ -1,16 +1,31 @@
 import {
     Grid,
-    TextField
+    TextField,
+    Button
 } from '@material-ui/core';
+import { useState } from 'react';
 
 export default function SendMessageForm() {
+    const [message, setMessage] = useState('');
+
+    const sendMessage = () => {
+        
+    }
     return (
         <Grid container>
-            <Grid item xs={12} style={{ padding: 10}}>
+            <Grid item xs={12} style={{ padding: 10 }}>
                 <TextField
                     variant="outlined"
-                    fullWidth
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
                 />
+                <Button
+                    variant="outlined"
+                    style={{ marginLeft: 5 }}
+                    onClick={sendMessage}
+                >
+                    Send
+                </Button>
             </Grid>
         </Grid>
     );
